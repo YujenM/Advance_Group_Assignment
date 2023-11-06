82dd7b4876465e0b942ac6137fa1a0fb085a6287
Yujen Maharjan <maharjanyuzen@gmail.com>
2023-11-06 12:19:30 +0545

    css fixed

--- src/main/java/com/example/advance_group/HelloApplication.java
+++ src/main/java/com/example/advance_group/HelloApplication.java
@@ -3,19 +3,36 @@ package com.example.advance_group;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    private double xOffset = 0;
+    private double yOffset = 0;
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Signup.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 750);
         stage.setTitle("Hello checking!");
         stage.initStyle(StageStyle.UNDECORATED);
+        stage.initStyle(StageStyle.UTILITY);
+        stage.setResizable(false);
         stage.setScene(scene);
+
+        scene.setOnMousePressed(event -> {
+            xOffset = event.getSceneX();
+            yOffset = event.getSceneY();
+        });
+
+        scene.setOnMouseDragged(event -> {
+            stage.setX(event.getScreenX() - xOffset);
+            stage.setY(event.getScreenY() - yOffset);
+        });
+
         stage.show();
     }
 
